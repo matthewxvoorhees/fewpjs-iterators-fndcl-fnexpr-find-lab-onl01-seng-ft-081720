f8087eadbedb3648c7c763f22630f4bb46343968
matthewxvoorhees <matthew.crnich@gmail.com>
2020-11-30 16:18:27 -0700

    commit tree 1c7362dc382025f7441f9447f56879035dd04229

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+  const found = record.find(element => element.result === "W")
+  if (found) {
+    return found.year
+  } 
+}
